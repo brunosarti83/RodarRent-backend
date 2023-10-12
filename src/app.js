e43135b7a5bddfa41c1a97dda7895f2417cb4611
brunosarti83 <brunosarti.bs@gmail.com>
2023-10-12 17:55:45 -0300

    deploy121023

--- src/app.js
+++ src/app.js
@@ -43,15 +43,6 @@ server.use(bodyParser.json({ limit: "50mb" }));
 //   })
 // );
 
-server.use(
-  cors({
-    origin: '*', //process.env.CLIENT_URL,
-    methods: 'GET,POST,PUT,DELETE',
-    credentials: true,
-  })
-);
-server.use(morgan("dev"));
-
 server.use((req, res, next) => {
   //res.header('Access-Control-Allow-Origin', process.env.CLIENT_URL); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
