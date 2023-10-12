c6853ec7f16eb7f87818be65055ff350364b185e
brunosarti83 <brunosarti.bs@gmail.com>
2023-10-12 10:24:28 -0300

    cors was duplicated

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const morgan = require("morgan");
 // const cookieParser = require("cookie-parser"); // now handled by express-session
 //const cookieSession = require("cookie-session"); // now handled by express-session
 const passport = require("passport");
-const cors = require("cors");
+//const cors = require("cors");
 const session = require("express-session");
 // routers
 const paymentsRouter = require("./routes/pay/paymentsRouter");
@@ -35,27 +35,27 @@ require("../auth"); // execute everything from auth.js (passport - google)
 server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 server.use(bodyParser.json({ limit: "50mb" }));
 
-server.use(
-  cors({
-    origin: '*', //process.env.CLIENT_URL,
-    methods: 'GET,POST,PUT,DELETE',
-    credentials: true,
-  })
-);
+// server.use(
+//   cors({
+//     origin: '*', //process.env.CLIENT_URL,
+//     methods: 'GET,POST,PUT,DELETE',
+//     credentials: true,
+//   })
+// );
 
 server.use(morgan("dev"));
 
-// server.use((req, res, next) => {
-//   //res.header('Access-Control-Allow-Origin', process.env.CLIENT_URL); // update to match the domain you will make the request from
-//   res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-//   res.header('Access-Control-Allow-Credentials', 'true');
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-//   next();
-// });
+server.use((req, res, next) => {
+  //res.header('Access-Control-Allow-Origin', process.env.CLIENT_URL); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
+});
 
 server.use("/", paymentsRouter);
 server.use("/", customersRouter);
